8a4460f2064116c5b15b69b8c077974d3ff32f92
Squirrellegend <jakubwj2@gmail.com>
2024-11-23 00:49:58 +0000

    OCR initial commit 97.5%

--- computer_vision.py
+++ computer_vision.py
@@ -1,7 +1,109 @@
 import cv2
 import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-IMG_PATH = "sudoku\\sudoku.png"
+
+WIDTH = 450
+HEIGHT = 450
+
+model = tf.keras.models.load_model("mnist_v01.keras")
 
 
-img = cv2.imread("")
+def sudoku_pre_processing(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
+    img_thresh = cv2.adaptiveThreshold(
+        img_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2
+    )
+    return img_thresh
+
+
+def cell_pre_processing(img):
+    crop = 7
+    img = img[crop:-crop, crop:-crop]
+    img = cv2.resize(img, (28, 28))
+    # img = cv2.equalizeHist(img)
+    img = img.reshape(img.shape[0], img.shape[1], 1)
+    img = img / 255
+    img = 1 - img
+    return img
+
+
+def largest_contour_area(contorus):
+    biggest = None
+    max_area = 0
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 1000:
+            peri = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+
+    return biggest, max_area
+
+
+def reorder_points(points):
+    points = points.reshape((4, 2))
+    new_points = np.zeros((4, 1, 2), dtype=np.int32)
+
+    add = points.sum(1)
+    new_points[0] = points[np.argmin(add)]
+    new_points[3] = points[np.argmax(add)]
+
+    diff = np.diff(points, axis=1)
+    new_points[1] = points[np.argmin(diff)]
+    new_points[2] = points[np.argmax(diff)]
+    return new_points
+
+
+def split_boxes(img):
+    boxes = []
+    for row in np.vsplit(img, 9):
+        for box in np.hsplit(row, 9):
+            boxes.append(box)
+    return boxes
+
+
+if __name__ == "__main__":
+    IMG_PATH = "sudoku\\sudoku_7_4.png"
+
+    img = cv2.imread(IMG_PATH)
+    # blank_image = np.zeros(img.shape, np.uint8)
+    thresh = sudoku_pre_processing(img)
+
+    contours, hierarchy = cv2.findContours(
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    largest_contour, area = largest_contour_area(contours)
+    img_with_sudoku = img.copy()
+    largest_contour = reorder_points(largest_contour)
+    # img_with_sudoku = cv2.drawContours(
+    #     img_with_sudoku, [largest_contour], -1, (0, 255, 0), 10
+    # )
+
+    pts1 = np.float32(largest_contour)
+    pts2 = np.float32([[0, 0], [WIDTH, 0], [0, HEIGHT], [WIDTH, HEIGHT]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    imgWarpColored = cv2.warpPerspective(img, matrix, (WIDTH, HEIGHT))
+    imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+
+    boxes = split_boxes(imgWarpGray)
+    cells = np.array(list(map(cell_pre_processing, boxes)))
+
+    predictions = model.predict(cells)
+    Y_pred_classes = np.argmax(predictions, axis=1)
+
+    print(Y_pred_classes.reshape(9, 9))
+
+    # for pred, cell in zip(Y_pred_classes, cells):
+    #     plt.imshow(cell, cmap="gray", vmin=0, vmax=1)
+    #     plt.axis("off")
+    #     plt.show()
+
+    plt.imshow(imgWarpGray, cmap="gray", vmin=0, vmax=255)
+    plt.axis("off")
+    plt.show()
